b3385542c317839bc9f7021ae2e5fd91416a522e
Davut Çiftçi <davutbciftci@gmail.com>
2026-03-09 17:32:01 +0300

    feat: Add Workbox-powered service worker for caching and offline capabilities.

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a87ad30a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.psg6b78pp1g"
+    "revision": "0.8rf4f56a7j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
